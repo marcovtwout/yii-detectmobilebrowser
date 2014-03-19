ec6ab73d0f2eae1b0aaf0cdb9e14387fdc346148
marcovtwout <mr@vtwout.net>
2014-03-19 10:46:52 +0100

    TXT: update readme location

--- XDetectMobileBrowser.php
+++ XDetectMobileBrowser.php
@@ -3,37 +3,12 @@
  * XDetectMobileBrowser class file.
  *
  * @author Marco van 't Wout <marco@tremani.nl>
- * @copyright Copyright &copy; Tremani, 2012
+ * @copyright Copyright &copy; Tremani, 2014
  */
 
 /**
  * Handles detecting mobile browsers.
- * Results are stored locally for caching calls within the same request, and
- * stored in a cookie for caching across requests.
- * Detection regex used from http://detectmobilebrowsers.com/
- *
- * Install as an application component, in your config:
- *
- * 'components' => array(
- *    'detectMobileBrowser' => array(
- *        'class' => 'ext.yii-detectmobilebrowser.XDetectMobileBrowser',
- *     ),
- * ),
- *
- * You can get the current user preference like this:
- *
- * if (Yii::app()->detectMobileBrowser->showMobile) {
- *     // do something
- * }
- *
- * By default it will use the automatically detected value.
- * You can also set the preference yourself like this:
- *
- * public function actionShowMobile() {
- *     Yii::app()->detectMobileBrowser->showMobile = true;
- *     $this->redirect(array('/site/index'));
- * }
- *
+ * @see README.md
  */
 class XDetectMobileBrowser extends CApplicationComponent
 {
